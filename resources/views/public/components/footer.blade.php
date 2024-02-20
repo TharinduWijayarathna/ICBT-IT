6e8145f9713eacb4a85dc549213c24f760650719
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-02-20 16:48:39 +0530

    UI fixes

--- resources/views/public/components/footer.blade.php
+++ resources/views/public/components/footer.blade.php
@@ -1,23 +1,20 @@
-<footer style="margin-top: 200px; margin-bottom: 20px;" class="footer py-5">
+<footer class="footer py-5">
     <div class="container pt-md-2 pt-lg-3 pt-xl-4">
         <div class="row pb-5 pt-sm-2 mb-lg-2">
             <div class="col-md-12 col-lg-3 pb-2 pb-lg-0 mb-4 mb-lg-0">
                 <a class="navbar-brand py-0 mb-3 mb-lg-4" href="index.html">
                     <span class="text-primary flex-shrink-0 me-2">
-                        <svg width="35" height="35" viewBox="0 0 48 48" fill="none"
+                        <svg version="1.1" width="35" height="32" viewBox="0 0 36 33"
                             xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M21 6H9C8.20435 6 7.44129 6.31607 6.87868 6.87868C6.31607 7.44129 6 8.20435 6 9V31C6 31.7956 6.31607 32.5587 6.87868 33.1213C7.44129 33.6839 8.20435 34 9 34H39C39.7956 34 40.5587 33.6839 41.1213 33.1213C41.6839 32.5587 42 31.7956 42 31V21M24 34V42"
-                                stroke="#FF0000" stroke-width="4" stroke-linecap="round"
-                                stroke-linejoin="round" />
-                            <path d="M32 6L28 10L32 14M38 6L42 10L38 14M14 42H34" stroke="#FF0000"
-                                stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                            <path fill="currentColor"
+                                d="M35.6,29c-1.1,3.4-5.4,4.4-7.9,1.9c-2.3-2.2-6.1-3.7-9.4-3.7c-3.1,0-7.5,1.8-10,4.1c-2.2,2-5.8,1.5-7.3-1.1c-1-1.8-1.2-4.1,0-6.2l0.6-1.1l0,0c0.6-0.7,4.4-5.2,12.5-5.7c0.5,1.8,2,3.1,3.9,3.1c2.2,0,4.1-1.9,4.1-4.2s-1.8-4.2-4.1-4.2c-2,0-3.6,1.4-4,3.3H7.7c-0.8,0-1.3-0.9-0.9-1.6l5.6-9.8c2.5-4.5,8.8-4.5,11.3,0L35.1,24C36,25.7,36.1,27.5,35.6,29z">
+                            </path>
                         </svg>
                     </span>
-                    <span class="text-nav">TechHive</span>
+                    <span class="text-nav">Around</span>
                 </a>
-                <p class="fs-sm pb-2 pb-lg-3 mb-3">Tellus non diam morbi quam vel venenatis proin sed. Dolor
-                    elementum nunc dictum</p>
+                <p class="fs-sm pb-2 pb-lg-3 mb-3">Tellus non diam morbi quam vel venenatis proin sed. Dolor elementum
+                    nunc dictum</p>
                 <div class="d-flex">
                     <a class="btn btn-icon btn-sm btn-secondary btn-facebook rounded-circle me-3" href="#"
                         aria-label="Facebook">
@@ -43,12 +40,9 @@
             </div>
             <div class="col-sm-4 col-lg-2 mb-4 mb-sm-0">
                 <ul class="nav flex-column">
-                    <li><a class="nav-link py-1 px-0" href="mailto:contact@example.com">contact@example.com</a>
-                    </li>
-                    <li><a class="nav-link py-1 px-0" href="tel:+15262200459">+1&nbsp;526&nbsp;220&nbsp;0459</a>
-                    </li>
-                    <li><a class="nav-link py-1 px-0" href="tel:+15262200444">+1&nbsp;526&nbsp;220&nbsp;0444</a>
-                    </li>
+                    <li><a class="nav-link py-1 px-0" href="mailto:contact@example.com">contact@example.com</a></li>
+                    <li><a class="nav-link py-1 px-0" href="tel:+15262200459">+1&nbsp;526&nbsp;220&nbsp;0459</a></li>
+                    <li><a class="nav-link py-1 px-0" href="tel:+15262200444">+1&nbsp;526&nbsp;220&nbsp;0444</a></li>
                 </ul>
             </div>
             <div class="col-sm-5 col-lg-4 col-xl-3 offset-lg-1">
@@ -56,14 +50,24 @@
                 <p class="fs-sm">Subscribe to our news and case studies</p>
                 <div class="input-group input-group-sm">
                     <input class="form-control" type="text" placeholder="Your email">
-                    <button class="btn btn-danger" type="button">Subscribe</button>
+                    <button class="btn btn-primary" type="button">Subscribe</button>
                 </div>
             </div>
         </div>
         <p class="nav fs-sm mb-0">
-            <span class="text-body-secondary">&copy;2024 All rights reserved. Made by</span>
-            <a class="nav-link fw-normal p-0 ms-1" href="https://createx.studio/" target="_blank"
-                rel="noopener">ICBT IT Society</a>
+            <span class="text-body-secondary">&copy; All rights reserved. Made by</span>
+            <a class="nav-link fw-normal p-0 ms-1" href="https://createx.studio/" target="_blank" rel="noopener">Createx
+                Studio</a>
         </p>
     </div>
 </footer>
+
+
+<!-- Back to top button -->
+<a class="btn-scroll-top" href="#top" data-scroll aria-label="Scroll back to top">
+    <svg viewBox="0 0 40 40" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="19" fill="none" stroke="currentColor" stroke-width="1.5"
+            stroke-miterlimit="10"></circle>
+    </svg>
+    <i class="ai-arrow-up"></i>
+</a>
--- resources/views/public/libraries/scripts.blade.php
+++ resources/views/public/libraries/scripts.blade.php
@@ -1,21 +1,10 @@
-<script src="{{ asset('assets/admin/vendor/libs/jquery/jquery.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/popper/popper.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/js/bootstrap.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/node-waves/node-waves.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/hammer/hammer.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/i18n/i18n.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/typeahead-js/typeahead.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/js/menu.js') }}"></script>
+<!-- Vendor scripts: .js')}} libraries and plugins -->
+<script src="{{ asset('assets/web/vendor/parallax-js/dist/parallax.min.js') }}"></script>
+<script src="{{ asset('assets/web/vendor/swiper/swiper-bundle.min.js') }}"></script>
+<script src="{{ asset('assets/web/vendor/aos/dist/aos.js') }}"></script>
 
+<!-- Bootstrap + Theme scripts -->
+<script src="{{ asset('assets/web/js/theme.min.js') }}"></script>
 
-
-<script src="{{ asset('assets/admin/vendor/libs/apex-charts/apexcharts.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/swiper/swiper.js') }}"></script>
-<script src="{{ asset('assets/admin/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-
-
-<script src="{{ asset('assets/admin/js/main.js') }}"></script>
-
-
-<script src="{{ asset('assets/admin/js/dashboards-analytics.js') }}"></script>
+<!-- Customizer -->
+<script src="{{ asset('assets/web/js/customizer.min.js') }}"></script>
