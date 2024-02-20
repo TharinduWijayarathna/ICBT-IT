84e64a377228b48396c4c6d6beb4a9d6f00fa89f
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-02-20 17:06:16 +0530

    UI Changes

--- resources/views/admin/components/navbar.blade.php
+++ resources/views/admin/components/navbar.blade.php
@@ -26,7 +26,7 @@
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="{{ asset('assets/img/avatars/1.png') }}" alt class="h-auto rounded-circle">
+                        <img src="{{ asset('assets/admin/img/avatars/1.png') }}" alt class="h-auto rounded-circle">
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
@@ -35,7 +35,7 @@
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
                                     <div class="avatar avatar-online">
-                                        <img src="{{ asset('assets/img/avatars/1.png') }}" alt
+                                        <img src="{{ asset('assets/admin/img/avatars/1.png') }}" alt
                                             class="h-auto rounded-circle">
                                     </div>
                                 </div>
