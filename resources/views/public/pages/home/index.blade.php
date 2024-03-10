722155c0d836308ea13ef1004d383e31e16636ef
Tharindu Wijayarathna <tharinduwijayarathne87@gmail.com>
2024-03-10 23:04:03 +0530

    Validations

--- resources/views/public/pages/home/index.blade.php
+++ resources/views/public/pages/home/index.blade.php
@@ -120,97 +120,95 @@
         </div>
     </section>
 
-
-
-
-    <!-- Case studies -->
-    <section class="container mb-5">
-        <div class="row align-items-center pt-md-4 mt-2 mt-sm-3 ">
-            <div class="col-md-6 position-relative pb-3 pb-md-0 mb-2 mb-sm-3 mb-md-0">
-                <div class="d-none d-xxl-block position-absolute"
-                    style="width: 861px; height: 719px; top: 50px; left: -260px;" data-aos="zoom-in" data-aos-duration="700"
-                    data-aos-offset="400">
-                    <svg class="text-primary opacity-10" width="861" height="719" viewBox="0 0 861 719"
-                        fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M25.1985 361.161C26.6142 363.592 28.4042 365.854 30.3902 367.846L115.89 453.613C117.366 455.093 119.008 456.457 120.69 457.697C220.882 531.542 166.267 694.09 321.314 716.256C481 739.09 730.799 588.084 806.496 453.151C844.188 385.962 885.87 273.92 840.453 199.651C808.854 147.97 742.627 142.437 686.285 142.442C616.832 142.453 556.116 167.459 491.294 135.468C449.5 114.831 426.769 82.5406 392.702 51.7928C206.459 -116.148 -88.0611 166.69 25.1985 361.161Z">
-                        </path>
-                    </svg>
-                </div>
-
-                <!-- Binded items -->
-                <div class="binded-content z-2">
-
-                    <!-- Item -->
-                    @foreach ($events as $event)
-                        <div class="binded-item active" id="image1">
-                            <img class="d-block rounded-5" src="{{ asset('event_images/' . $event->image->name) }}"
-                                alt="Image">
-                        </div>
-                    @endforeach
-
-
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-5 offset-xl-1">
-                <div class="ps-md-4 ps-xl-0">
-
-                    <!-- Slider control buttons (Prev / Next) -->
-                    <div class="d-flex align-items-center ms-n3 pb-3 mb-sm-2 mb-xl-4">
-                        <button class="btn btn-icon btn-link" type="button" id="prev-case-study" aria-label="Prev">
-                            <i class="ai-arrow-left"></i>
-                        </button>
-                        <div class="text-center text-nowrap mx-3" id="slides-count" style="width: 33px;"></div>
-                        <button class="btn btn-icon btn-link" type="button" id="next-case-study" aria-label="Next">
-                            <i class="ai-arrow-right"></i>
-                        </button>
+    @if (count($events) > 0)
+        <section class="container mb-5">
+            <div class="row align-items-center pt-md-4 mt-2 mt-sm-3 ">
+                <div class="col-md-6 position-relative pb-3 pb-md-0 mb-2 mb-sm-3 mb-md-0">
+                    <div class="d-none d-xxl-block position-absolute"
+                        style="width: 861px; height: 719px; top: 50px; left: -260px;" data-aos="zoom-in" data-aos-duration="700"
+                        data-aos-offset="400">
+                        <svg class="text-primary opacity-10" width="861" height="719" viewBox="0 0 861 719"
+                            fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M25.1985 361.161C26.6142 363.592 28.4042 365.854 30.3902 367.846L115.89 453.613C117.366 455.093 119.008 456.457 120.69 457.697C220.882 531.542 166.267 694.09 321.314 716.256C481 739.09 730.799 588.084 806.496 453.151C844.188 385.962 885.87 273.92 840.453 199.651C808.854 147.97 742.627 142.437 686.285 142.442C616.832 142.453 556.116 167.459 491.294 135.468C449.5 114.831 426.769 82.5406 392.702 51.7928C206.459 -116.148 -88.0611 166.69 25.1985 361.161Z">
+                            </path>
+                        </svg>
                     </div>
 
-                    <!-- Swiper slider -->
-                    <div class="swiper"
-                        data-swiper-options='{
-          "spaceBetween": 40,
-          "autoHeight": true,
-          "bindedContent": true,
-          "pagination": {
-            "el": "#slides-count",
-            "type": "fraction"
-          },
-          "navigation": {
-            "prevEl": "#prev-case-study",
-            "nextEl": "#next-case-study"
-          }
-        }'>
-                        <div class="swiper-wrapper">
+                    <!-- Binded items -->
+                    <div class="binded-content z-2">
 
-                            @foreach ($events as $event)
-                                <!-- Event -->
-                                <div class="swiper-slide pb-1" data-swiper-binded="#eventImage1">
-                                    <h3 class="h4">{{ $event->title }}</h3>
-                                    <p class="pb-3 mb-3 mb-xl-4">{{ $event->description }}</p>
-                                    <div class="d-flex d-md-none d-lg-flex justify-content-between w-100 pb-4 pb-xl-5 mb-2 mb-sm-3 mb-xl-2"
-                                        style="max-width: 550px;">
-                                        <div class="pe-4">
-                                            <div class="h6 mb-1">Date: {{ date('d M Y', strtotime($event->start_date)) }}
-                                                - {{ date('d M Y', strtotime($event->end_date)) }}</div>
-                                            {{-- <div class="fs-sm">Time: 10:00 AM - 5:00 PM</div> --}}
+                        <!-- Item -->
+                        @foreach ($events as $event)
+                            <div class="binded-item active" id="image1">
+                                <img class="d-block rounded-5" src="{{ asset('event_images/' . $event->image->name) }}"
+                                    alt="Image">
+                            </div>
+                        @endforeach
+
+
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-5 offset-xl-1">
+                    <div class="ps-md-4 ps-xl-0">
+
+                        <!-- Slider control buttons (Prev / Next) -->
+                        <div class="d-flex align-items-center ms-n3 pb-3 mb-sm-2 mb-xl-4">
+                            <button class="btn btn-icon btn-link" type="button" id="prev-case-study" aria-label="Prev">
+                                <i class="ai-arrow-left"></i>
+                            </button>
+                            <div class="text-center text-nowrap mx-3" id="slides-count" style="width: 33px;"></div>
+                            <button class="btn btn-icon btn-link" type="button" id="next-case-study" aria-label="Next">
+                                <i class="ai-arrow-right"></i>
+                            </button>
+                        </div>
+
+                        <!-- Swiper slider -->
+                        <div class="swiper"
+                            data-swiper-options='{
+      "spaceBetween": 40,
+      "autoHeight": true,
+      "bindedContent": true,
+      "pagination": {
+        "el": "#slides-count",
+        "type": "fraction"
+      },
+      "navigation": {
+        "prevEl": "#prev-case-study",
+        "nextEl": "#next-case-study"
+      }
+    }'>
+                            <div class="swiper-wrapper">
+
+                                @foreach ($events as $event)
+                                    <!-- Event -->
+                                    <div class="swiper-slide pb-1" data-swiper-binded="#eventImage1">
+                                        <h3 class="h4">{{ $event->title }}</h3>
+                                        <p class="pb-3 mb-3 mb-xl-4">{{ $event->description }}</p>
+                                        <div class="d-flex d-md-none d-lg-flex justify-content-between w-100 pb-4 pb-xl-5 mb-2 mb-sm-3 mb-xl-2"
+                                            style="max-width: 550px;">
+                                            <div class="pe-4">
+                                                <div class="h6 mb-1">Date: {{ date('d M Y', strtotime($event->start_date)) }}
+                                                    - {{ date('d M Y', strtotime($event->end_date)) }}</div>
+                                                {{-- <div class="fs-sm">Time: 10:00 AM - 5:00 PM</div> --}}
+                                            </div>
+                                        </div>
+                                        <div class="d-sm-flex">
+                                            <a class="btn btn-outline-primary w-100 w-sm-auto d-md-none d-lg-inline-flex"
+                                                href="{{ route('society.events') }}">View All Events</a>
                                         </div>
                                     </div>
-                                    <div class="d-sm-flex">
-                                        <a class="btn btn-outline-primary w-100 w-sm-auto d-md-none d-lg-inline-flex"
-                                            href="{{ route('society.events') }}">View All Events</a>
-                                    </div>
-                                </div>
-                            @endforeach
+                                @endforeach
 
 
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        @endif
 
     <section class="container pt-5 mt-lg-3 mt-xl-4 mt-xxl-5 mb-5">
     </section>
